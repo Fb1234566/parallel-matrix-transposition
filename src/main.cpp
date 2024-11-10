a653644bde31cffa6a07f4134194518d9232e242
Filippo Benedetti <filippo.benedetti3@gmail.com>
2024-11-10 20:12:45 +0100

    Remove useless prints, add passage of parameters, add execution time measurement

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,114 @@
 #include <iostream>
 #include "../include/utility.h"
 #include <cstdlib>
-
-#define SIZE 4 // size of the matrix
-
-float M [SIZE][SIZE]; // original matrix
-float M_transposed [SIZE][SIZE]; // transposed matrix
+#include <cstring>
+#include <sstream>
 
 void matTranspose (const float* m, float* m_transposed, const unsigned int size) {
-    // transposes the matrix
-    std::cout<<"Transposing matrix..."<<std::endl;
+/*
+    Transposes square matrix
+    Parameters:
+        - m: matrix to transpose
+        - m_transposed: matrix used to save the transposition of m
+        - size: size of the square matrix
+*/
     for (unsigned int i = 0; i < size; i++) {
         for (unsigned int j = 0; j < size; j++) {
             m_transposed[j+i*size] = m[i+j*size];
         }
     }
-    std::cout<<"Finished transposing!"<<std::endl;
 }
 
-bool checkSym (float* m, const unsigned int size) {
-    // checks that the matrix is symmetric
-    std::cout<<"Checking symmetry matrix..."<<std::endl;
-    matTranspose(reinterpret_cast<float *>(M), reinterpret_cast<float *>(M_transposed), SIZE);
-    if (checkEqual(reinterpret_cast<float *>(M), reinterpret_cast<float *>(M_transposed), SIZE, SIZE)) {
-        std::cout<<"Matrix is symmetric!"<<std::endl;
+bool checkSym (const float* m, const unsigned int size) {
+/*
+    Checks if a square matrix is symmetric
+    Parameters:
+        - m: matrix to check for symmetry
+        - size: dimensions of the matrix.
+*/
+
+    float m_transposed[size][size];
+    matTranspose(m, reinterpret_cast<float * >(m_transposed), size);
+    if (checkEqual(m, reinterpret_cast<float *>(m_transposed), size, size)) {
         return true;
     }
-    std::cout<<"Matrix is not symmetric!"<<std::endl;
     return false;
 }
 
 
 
-int main() {
-    srand(time(NULL));
-    initialize_matrix(reinterpret_cast<float *>(M), SIZE, SIZE);
-    bool res = checkSym((float*) M, SIZE);
+int main(int argc, char* argv[]) {
+    srand(time(nullptr)); // sets the random seed
+    bool command = false; // used to check if a command has been passed via parameters
+    int size = -1;
+
+    // take input parameters
+    if (argc == 1) {
+        // checks if there are no arguments
+        std::cerr << "Missing arguments!" << std::endl;
+        return -1;
+    }
+
+    // iterate through argv
+    for(unsigned int i = 0; i<=argc; i++) {
+        if (command) { // check if prev argument is a command
+            if (strcmp(argv[i-1], "-s")==0) { // do action if command is found
+                // check if the input is an actual number
+                try {
+                    size = std::stoi(argv[i]);
+                    if (size < 1) {
+                        std::cerr << "Invalid size!" << std::endl;
+                        return -1;
+                    }
+                } catch (...) {
+                    std::cerr << "Invalid size!" << std::endl;
+                    return -1;
+                }
+                command = false;
+            }
+            if (strcmp(argv[i-1], "-h")==0) {
+                std::cout << "Usage: " << std::endl;
+                std::cout << "\t-s <size>: Size of the square matrix to be transposed" << std::endl;
+                std::cout << "\t-h: Print this help message" << std::endl;
+                return 0;
+            }
+        }
+        else {
+            if (i == argc) {
+                break;
+            }
+            if(argv[i][0] == '-') { // check if currently parsed arg is a command
+                if((argv[i][1] == 's') || (argv[i][1] == 'h')) {
+                    command = true;
+                }
+                else { // if command is valid
+                    std::cerr << "Invalid argument: " << argv[i] << std::endl;
+                    return -1;
+                }
+            }
+        }
+    }
+
+    if (size == -1) {
+        std::cerr << "Size not set: pass -s to set the size of the matrix" << std::endl;
+        return -1;
+    }
+
+    float M[size][size];
+    initialize_matrix(reinterpret_cast<float *>(M), size, size);
+    std::cout << "Parallel Matrix Transposition" << std::endl;
+    std::cout << "-----------------------------" << std::endl;
+    std::cout << "[1] Parallel section:" << std::endl;
+    const clock_t t0_seq = clock();
+    const bool sym = checkSym(reinterpret_cast<float *>(M), size);
+    const clock_t t1_seq = clock();
+    if (sym) {
+        std::cout<<"Matrix is symmetric!"<<std::endl;
+    }
+    else {
+        std::cout<<"Matrix is not symmetric!"<<std::endl;
+    }
+
+    std::cout << "Time: " << (static_cast<double>(t1_seq - t0_seq)) / CLOCKS_PER_SEC << " sec" << std::endl;
     return 0;
 }
--- src/utility.cpp
+++ src/utility.cpp
@@ -12,28 +12,22 @@ float rand_float(int min, int max) {
 
 void initialize_matrix(float* m, const unsigned int rows, const unsigned int cols) {
     // initializes the matrix with random values withing 0 and 100
-    std::cout<<"Initializing matrix..."<<std::endl;
     for( int i = 0; i < rows; ++i) {
         for( int j = 0; j < cols; ++j) {
             m[j+i*cols] = rand_float(0, 1000);
         }
     }
-
-    std::cout<<"Matrix initialized!"<<std::endl;
 }
 
 bool checkEqual(const float* M1, const float* M2, const unsigned int rows, const unsigned int cols) {
     // Function to check if the two matrices are equal
-    std::cout<<"Checking equality..."<<std::endl;
     for (unsigned int i = 0; i < rows; ++i) {
         for (unsigned int j = 0; j < cols; ++j) {
             if (M1[j+i*cols] != M2[j+i*cols]) {
-                std::cout<<"The two matrixes are not equal!"<<std::endl;
                 return false;
             }
         }
     }
-    std::cout<<"The two matrixes are equal!"<<std::endl;
     return true;
 }
 
