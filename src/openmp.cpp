2c710e8ec0583a92931d6d3fbad43c02bdbeaba3
Filippo Benedetti <filippo.benedetti3@gmail.com>
2024-11-30 00:10:30 +0100

    Change scheduling parameters

--- src/openmp.cpp
+++ src/openmp.cpp
@@ -2,7 +2,7 @@
 #include "../include/openmp.h"
 #include <omp.h>
 
-std::vector<std::vector<float>> matTransposeOMP (const std::vector<std::vector<float>> &m, const unsigned int size, int threads, const int chunkSize) {
+std::vector<std::vector<float>> matTransposeOMP (const std::vector<std::vector<float>> &m, const unsigned int size, int threads) {
     /*
         Transposes square matrix
         Parameters:
@@ -13,7 +13,7 @@ std::vector<std::vector<float>> matTransposeOMP (const std::vector<std::vector<f
     std::vector<std::vector<float>> m_transposed (size, std::vector<float>(size));
     omp_set_dynamic(0);
     omp_set_num_threads(threads);
-    #pragma omp parallel for collapse(2) schedule(dynamic, chunkSize)
+    #pragma omp parallel for collapse(2) schedule(dynamic)
     for (unsigned int i = 0; i < size; i++) {
         for (unsigned int j = 0; j < size; j++) {
             m_transposed[i][j] = m[j][i];
